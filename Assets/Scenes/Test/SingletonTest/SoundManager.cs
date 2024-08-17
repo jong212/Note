37ece54b5859203e97d8725d55cb252290777b96
jonghwa <jonghwa0212@gmail.com>
2024-08-18 03:21:10 +0900

    [singleton] 제네릭 싱글톤 코드 저장
    Scenes > Test > SingletonTest 폴더의 TestScene1 씬 실행하여 싱글톤 적용되는지 확인 가능

---
+++ Assets/Scenes/Test/SingletonTest/SoundManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : Singleton<SoundManager>
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Test()
+    {
+        Debug.Log("gdg");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
